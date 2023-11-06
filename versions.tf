50bdbe4467cb33bc2edfbe5b1c4775bd1c2df314
Leandro Gonçalves de Oliveira <olegon.main@gmail.com>
2023-11-06 16:46:50 -0300

    chore: enhancing version contrains;

--- versions.tf
+++ versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1"
+  required_version = ">= 1.0.0, < 2.0.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5, < 6"
+      version = ">= 5.0.0, < 6.0.0"
     }
 
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2, < 3"
+      version = ">= 2.0.0, < 3.0.0"
     }
   }
 }
