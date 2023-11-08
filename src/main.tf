af63bacb4b50c6eeb6275eb48d99c7c798e2c370
Leandro Gonçalves de Oliveira <olegon.main@gmail.com>
2023-11-07 22:57:38 -0300

    chore: moving API GW to soat1-grupo18/fase3-infra-apigateway repository;

--- src/main.tf
+++ src/main.tf
@@ -22,11 +22,6 @@ provider "helm" {
     host                   = aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.this.token
-    # exec {
-    #   api_version = "client.authentication.k8s.io/v1beta1"
-    #   args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name]
-    #   command     = "aws"
-    # }
   }
 }
 
