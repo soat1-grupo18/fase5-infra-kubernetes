f470628e532b10ba2db91c49c18c22c3d04e9602
Pablo Lima Dias <pabloidz@gmail.com>
2024-03-13 21:41:38 -0300

    Update init-aws.sh

--- localstack/init-aws.sh
+++ localstack/init-aws.sh
@@ -6,7 +6,11 @@ awslocal sns create-topic \
   --name ms-pedido
 
 awslocal sqs create-queue \
-  --queue-name ms-pagamento-evento-pedido-recebido
+  --queue-name ms-pagamento-evento-pedido-recebido-dlq
+
+awslocal sqs create-queue \
+  --queue-name ms-pagamento-evento-pedido-recebido \
+  --attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\": \"arn:aws:sqs:us-east-1:000000000000:ms-pagamento-evento-pedido-recebido-dlq\", \"maxReceiveCount\": \"3\" }" }'
 
 awslocal sns subscribe \
   --topic-arn arn:aws:sns:us-east-1:000000000000:ms-pedido \
@@ -15,10 +19,45 @@ awslocal sns subscribe \
   --attributes '{ "RawMessageDelivery": "true" }'
 
 awslocal sqs create-queue \
-  --queue-name ms-producao-evento-pedido-recebido
+  --queue-name ms-producao-evento-pedido-recebido-dlq
+
+awslocal sqs create-queue \
+  --queue-name ms-producao-evento-pedido-recebido \
+  --attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\": \"arn:aws:sqs:us-east-1:000000000000:ms-producao-evento-pedido-recebido-dlq\", \"maxReceiveCount\": \"3\" }" }'
 
 awslocal sns subscribe \
   --topic-arn arn:aws:sns:us-east-1:000000000000:ms-pedido \
   --protocol sqs \
   --notification-endpoint arn:aws:sqs:us-east-1:000000000000:ms-producao-evento-pedido-recebido \
   --attributes '{ "RawMessageDelivery": "true" }'
+
+############################################
+
+awslocal sns create-topic \
+  --name ms-pagamento
+
+awslocal sqs create-queue \
+  --queue-name ms-pagamento-evento-pagamento-aprovado-dlq
+
+awslocal sqs create-queue \
+  --queue-name ms-pagamento-evento-pagamento-aprovado \
+  --attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\": \"arn:aws:sqs:us-east-1:000000000000:ms-pagamento-evento-pagamento-aprovado-dlq\", \"maxReceiveCount\": \"3\" }" }'
+
+awslocal sns subscribe \
+  --topic-arn arn:aws:sns:us-east-1:000000000000:ms-pagamento \
+  --protocol sqs \
+  --notification-endpoint arn:aws:sqs:us-east-1:000000000000:ms-pagamento-evento-pagamento-aprovado \
+  --attributes '{ "RawMessageDelivery": "true", "FilterPolicy": "{\"event-type\": [\"aprovado\"]}" }'
+
+awslocal sqs create-queue \
+  --queue-name ms-pagamento-evento-pagamento-recusado-dlq
+
+awslocal sqs create-queue \
+  --queue-name ms-pagamento-evento-pagamento-recusado \
+  --attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\": \"arn:aws:sqs:us-east-1:000000000000:ms-pagamento-evento-pagamento-recusado-dlq\", \"maxReceiveCount\": \"3\" }" }'
+
+awslocal sns subscribe \
+  --topic-arn arn:aws:sns:us-east-1:000000000000:ms-pagamento \
+  --protocol sqs \
+  --notification-endpoint arn:aws:sqs:us-east-1:000000000000:ms-pagamento-evento-pagamento-recusado \
+  --attributes '{ "RawMessageDelivery": "true", "FilterPolicy": "{\"event-type\": [\"recusado\"]}" }'
